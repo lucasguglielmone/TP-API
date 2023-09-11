d9060b8a121cba3a59d327a3b960a4305d3ec7ac
Ian Campo <ian.campo@claro.com.ar>
2023-09-11 11:24:52 -0300

    Version con SQLite, en principio funciona. En aplication.properties hay que agregar la direccion del .db, que lo deje en la carpeta q se llama ´db_sqlite´. Te la dedico Godio!

--- src/main/java/com/example/demo/ApiFinalApplication.java
+++ src/main/java/com/example/demo/ApiFinalApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiTestApplication {
+public class ApiFinalApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiTestApplication.class, args);
+		SpringApplication.run(ApiFinalApplication.class, args);
 	}
 
 }
